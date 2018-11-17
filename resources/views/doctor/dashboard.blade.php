213bc596383b0eae894325c0e8e1754611cb63c1
Emmanuel Paul <e.paul@dreammesh.ng>
2018-11-17 03:57:32 +0100

    updates

---
+++ resources/views/doctor/dashboard.blade.php
@@ -0,0 +1,68 @@
+@extends('base')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="content-area overflow-hidden">
+
+        <div class="page-header" style="margin-bottom: -10px;">
+            <h4 class="page-title">Patient's Dashboard</h4>
+        </div>
+        @if ($EditProfile == "set")
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="alert alert-warning d-none d-lg-block" role="alert">
+                        <button type="button" class="close text-white" data-dismiss="alert" aria-hidden="true">×</button>
+                    Hi <strong>{{$sessiondata['firstname']}}</strong>, you have only completed 30% of your profile.
+                        <a href="/edit-user" class="btn btn-white btn-sm float-right mr-2">Complete My Profile Now</a>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        <div class="row row-cards">
+            <div class="col-sm-12 col-lg-12 col-xl-4">
+                <div class="row">
+                    <div class="col-sm-12 col-lg-12">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div class=" wx">
+                                    <h1 class="">&#8358;0.00</h1>
+                                    <p class="text-muted mb-0 lead"><span class="text-green"><i class="fa fa-arrow-circle-o-up text-green"></i></span> Calling Balance</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-lg-12">
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <div class=" wx">
+                                    <h1 class="">&#8358;0.00</h1>
+                                    <p class="text-muted mb-0 lead"><span class="text-red"><i class="fa fa-arrow-circle-o-down text-red"></i></span> Total Used-up</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-sm-12 col-lg-12 col-xl-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Medical Aid Stats</h3>
+                        <div class="card-options">
+                            <a href="#" class="card-options-collapse" data-toggle="card-collapse"><i class="fe fe-chevron-up"></i></a>
+                            <a href="#" class="card-options-remove" data-toggle="card-remove"><i class="fe fe-x"></i></a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="chart-container">
+                            <div id="echart2" class="chart-tasks dropshadow"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
---
+++ resources/views/doctor/sidebar.blade.php
@@ -0,0 +1,90 @@
+<nav id="sidebar" class="nav-sidebar">
+        <div class="profile clearfix">
+            <div class="profile_pic">
+                <img src="{{$sessiondata['avatar']}}" alt="..." class="img-circle profile_img">
+            
+            </div>
+            <div class="profile_info">
+            <h2>{{$sessiondata['firstname']}} {{$sessiondata['lastname']}}</h2>
+            <span class="label label-success" style="color: #000; font-size: bold;">{{ $sessiondata['role'] }}</span>
+            </div>
+        </div>
+        
+        <ul class="list-unstyled components" id="accordion">
+           
+            <li>
+                <a href="/dashboard" class="accordion-toggle  wave-effect" aria-expanded="false">
+                    <i class="fa fa-television mr-2 sidebarsuccess"></i>Dashboard
+                </a>
+               
+            </li>
+
+            <li class="border-0"><h3>Personal Information</h3><li>
+            <li>
+                <a href="/" class=" wave-effect accordion-toggle "><i class="fa fa-eye mr-2 sidebarpink"></i> View Profile</a>
+            </li>
+            <li>
+                <a href="/edit-user" class=" wave-effect accordion-toggle "><i class="fa fa-pencil mr-2 sidebarpink"></i>Update Profile</a>
+            </li>
+
+            <li class="border-0"><h3>Payment Methods</h3><li>
+            <li>
+                <a href="#" class=" wave-effect accordion-toggle "><i class="fa fa-money mr-2 sidebarpink"></i> Add Debit/Credit Card</a>
+            </li>
+            <li>
+                <a href="#" class=" wave-effect accordion-toggle "><i class="fa fa-file mr-2 sidebarpink"></i>Transaction History</a>
+            </li>
+
+            <li><h3>Subscription</h3><li>
+
+            <li>
+                <a href="#Submenu9" class="accordion-toggle wave-effect" data-toggle="collapse" aria-expanded="false">
+                    <i class="fa fa-futbol-o mr-2 sidebarred"></i> My Subscription
+                </a>
+                <ul class="collapse list-unstyled" id="Submenu9" data-parent="#accordion">
+                    <li>
+                        <a href="#">Choose Subscription Plan</a>
+                    </li>
+                    <li>
+                        <a href="#">Subscription History</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li><h3>Medical Need</h3><li>
+
+            <li>
+                <a href="#" class=" wave-effect accordion-toggle"><i class="fa fa-ticket mr-2 sidebarlightgreen"></i>  Schedule Appointments</a>
+            </li>
+
+            <li><h3>More</h3><li>
+
+            <li>
+                <a href="#" class=" wave-effect accordion-toggle"><i class="fa fa-ticket mr-2 sidebarlightgreen"></i>  Rate Doctor</a>
+            </li>
+            <li>
+                <a href="#" class=" wave-effect accordion-toggle"><i class="fa fa-ticket mr-2 sidebarlightgreen"></i>  Support</a>
+            </li>
+            <li>
+                <a href="#" class=" wave-effect accordion-toggle"><i class="fa fa-ticket mr-2 sidebarlightgreen"></i>  Terms of Use</a>
+            </li>
+        </ul>
+
+        <div class="sidebar-footer hidden-small">
+            <a href="emailservices.html">
+                <span class="fa fa-envelope" aria-hidden="true"></span>
+            </a>
+            <a href="profile.html">
+                <span class="fa fa-user" aria-hidden="true"></span>
+            </a>
+            <a href="editprofile.html">
+                <span class="fa fa-cog" aria-hidden="true"></span>
+            </a>
+            <a href="login.html">
+                <span class="fa fa-sign-in" aria-hidden="true"></span>
+                </a>
+            <a href="/logout">
+                <span class="fa fa-power-off" aria-hidden="true"></span>
+            </a>
+        </div>
+    </nav>
